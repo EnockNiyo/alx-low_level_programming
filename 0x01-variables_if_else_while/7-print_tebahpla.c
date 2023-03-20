0b82a7e0b2533da3225b05491673c3c7aafe6184
EnockNiyo <niyomwungerienock@gmail.com>
2023-03-20 03:35:58 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * rite a program that prints the lowercase alphabet in reverse, followed by a new line.
+ * main - main block
+ * Description: prints all single digit numbers of base 10
  * starting from 0, followed by a new line.
  * Return: 0
  */
