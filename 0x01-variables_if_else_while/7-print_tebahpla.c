26362ae80fec81f19160916f57d46a30b7c8a385
EnockNiyo <niyomwungerienock@gmail.com>
2023-03-20 03:30:33 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
