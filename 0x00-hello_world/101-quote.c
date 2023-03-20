fe2b42931d721657d3459a06492fc8a84dd71b7f
EnockNiyo <niyomwungerienock@gmail.com>
2023-03-20 02:28:04 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point of program
+ *
+ *Return: Always 0 - successful
+ */
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fputs(str, stderr);
+
+return (1);
+}
