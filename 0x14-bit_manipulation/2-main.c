e0d37031cc24fb241f648f11d3acbd7814090505
EnockNiyo <niyomwungerienock@gmail.com>
2023-05-20 23:20:16 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    int n;
+    int n;
 
-	        n = get_bit(1024, 10);
-		    printf("%d\n", n);
-		        n = get_bit(98, 1);
-			    printf("%d\n", n);
-			        n = get_bit(1024, 0);
-				    printf("%d\n", n);
-				        return (0);
+    n = get_bit(1024, 10);
+    printf("%d\n", n);
+    n = get_bit(98, 1);
+    printf("%d\n", n);
+    n = get_bit(1024, 0);
+    printf("%d\n", n);
+    return (0);
 }
